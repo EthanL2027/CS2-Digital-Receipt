36696f5c9325c925a6087d0ff2f7a01015cc3c20
Ethan <Ethan.lynn2027@gbwl.org>
2025-10-24 12:55:55 +0000

    Part B

--- Main.java
+++ Main.java
@@ -1,6 +1,33 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      System.out.println("Receipt");
+
+     String highSchoolName = "Crenshaw";
+     double drinkPrice = 1.50;
+     double candyPrice = 1.25;
+     double hotdogPrice = 2.75;
+     double hamburgerPrice = 3.50;
+     
+     double orderNumber = (int) ( Math.random() * 100)+1;
+      
+   
+      
+     
+     
+      // RECEIPT PRINTOUT SECTION
+     System.out.println("**************************************");
+     System.out.println("*                                    *");
+     System.out.println("*     " + highSchoolName + " Snack Bar             *");
+     System.out.println("*                                    *");
+     System.out.println("*     Drink ..........$" + drinkPrice + "           *");                      
+     System.out.println("*     Candy ..........$" + candyPrice + "          *");     
+     System.out.println("*     Hot Dog ........$" + hotdogPrice + "          *");     
+     System.out.println("*     Hamburger ......$" + hamburgerPrice + "           *");     
+     System.out.println("*                                    *");    
+     System.out.println("**************************************");
+
+
+
    }
 }
